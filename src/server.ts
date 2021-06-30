71f4ffd58642a3464f2b702f8d8bac0836b34090
Herbert Paes <herbert_pbarreto@hotmail.com>
2021-06-30 09:33:23 -0300

    Criação do database

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,8 @@
+import 'reflect-metadata';
 import express from 'express';
 
+import './database';
+
 const app = express();
-
-app.get('/', (req , res) => {
-    return res.send("Olá get");
-})
-
-
-app.post('/', (req, res) =>{
-    return res.send("Olá Post")
-})
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
